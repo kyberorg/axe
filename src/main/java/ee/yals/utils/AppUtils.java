bcc83986e17df356906046be5bf7bb7834d447b4
Alex Muravya <asm@virtalab.net>
2017-07-18 07:42:19 +0300

    Enabling JVM Remote debugging (#5)

--- src/main/java/ee/yals/utils/AppUtils.java
+++ src/main/java/ee/yals/utils/AppUtils.java
@@ -29,13 +29,8 @@ public class AppUtils {
             throw new UnsupportedOperationException("Utility class");
         }
 
-        public static String getHostFromRequest(HttpServletRequest request) {
-            boolean isAppRunningInDocker = StringUtils.isNoneBlank((System.getProperty("docker.internal.port", "")));
-            if (isAppRunningInDocker) {
-                return "localhost" + ":" + System.getProperty("docker.internal.port");
-            } else {
-                return request.getServerName() + ":" + request.getServerPort();
-            }
+        public static String getAPIHostPort() {
+            return "localhost" + ":" + System.getProperty("server.port", "8080");
         }
     }
 }
