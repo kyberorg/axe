a5184003f4016641974e1f15b520fbe74337313a
Alexander Muravya <asm@virtalab.net>
2017-12-20 14:22:05 +0200

    #26 Test written
    Signed-off-by: Alexander Muravya <asm@virtalab.net>

--- src/test/java/ee/yals/test/selenide/front/MultiStepTest.java
+++ src/test/java/ee/yals/test/selenide/front/MultiStepTest.java
@@ -3,10 +3,12 @@ package ee.yals.test.selenide.front;
 import ee.yals.test.selenide.UITest;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Keys;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Contains multi step tests for Front page
@@ -48,6 +50,22 @@ public class MultiStepTest extends UITest {
         $("div#result").shouldNotBe(visible);
         $("#resultLink").shouldBe(empty);
 
+    }
+
+    @Test
+    public void copyLinkButtonShouldCopyShortLink() {
+        String longLink = "https://github.com/yadevee/yals";
+        pasteValueInFormAndSubmitIt(longLink);
+
+        $("div#result").shouldBe(visible);
+        $("#copyLink").shouldBe(visible);
+
+        $("#copyLink").click();
+        $("#longUrl").click();
+        $("#longUrl").sendKeys(Keys.chord(Keys.CONTROL, "v"));
+
+        String pastedText = $("#longUrl").text();
+        assertEquals(longLink, pastedText);
     }
 
     private void pasteValueInFormAndSubmitIt(String link) {
