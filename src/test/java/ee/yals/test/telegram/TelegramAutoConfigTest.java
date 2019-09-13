aabc008d3f56ab92c7ab849863a905f1d586161e
Alex Muravya <asm@virtalab.net>
2019-09-13 21:58:00 +0300

    test

--- src/test/java/ee/yals/test/telegram/TelegramAutoConfigTest.java
+++ src/test/java/ee/yals/test/telegram/TelegramAutoConfigTest.java
@@ -1,5 +1,12 @@
 package ee.yals.test.telegram;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import ee.yals.Endpoint;
+import ee.yals.json.LinkResponseJson;
+import ee.yals.json.TelegramStatusResponseJson;
+import ee.yals.test.utils.Selenide;
+import ee.yals.utils.AppUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,8 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 
 /**
@@ -22,22 +28,37 @@ import static org.junit.Assert.assertNotNull;
 @Slf4j
 public class TelegramAutoConfigTest extends TelegramTest {
 
-    //disabled as "bot can't send messages to bots"
-    //@Test
+
+    @Test
     public void sendStartCommandGivesNonEmptyReply() {
         //TODO check status
-        if (isCorrectlyInitialized()) {
-            String message = "hello";
-            String reply = sendMessageToBot(message);
-            assertNotNull(reply);
-            assertNotEquals(EMPTY_RESPONSE, reply);
+        if (isLocalRun()) {
+            log.info("Local run: telegram bot never started");
+            return;
+        }
+
+        String statusApiEndpoint = String.format("%s%s", System.getProperty(Selenide.Props.TEST_URL),
+                Endpoint.TELEGRAM_STATUS_API);
+        HttpResponse<String> apiResponse;
+        try {
+            apiResponse = Unirest.get(statusApiEndpoint).asString();
+        } catch (Exception e) {
+            log.error("Failed to Request API. Communication error", e);
+            return;
+        }
+
+        if (apiResponse.getStatus() == 200) {
+            String status = extractBotStatus(apiResponse);
+            assertTrue(status.contains("Online"));
         } else {
-            log.info("No token - no action");
+            log.error("Api Request failed with status {}", apiResponse.getStatus());
+            log.debug("API response: {}", apiResponse);
         }
     }
 
-    @Test
-    public void tmpEmptyTest() {
-        //all good
+    private String extractBotStatus(HttpResponse<String> apiResponse) {
+        TelegramStatusResponseJson json = AppUtils.GSON.fromJson(apiResponse.getBody(), TelegramStatusResponseJson.class);
+        return json.getStatus();
     }
+
 }
--- src/test/java/ee/yals/test/telegram/TelegramTest.java
+++ src/test/java/ee/yals/test/telegram/TelegramTest.java
@@ -1,5 +1,7 @@
 package ee.yals.test.telegram;
 
+import ee.yals.test.utils.Selenide;
+import ee.yals.utils.AppUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.TestPropertySource;
@@ -19,58 +21,13 @@ import java.net.URLConnection;
 @TestPropertySource(locations = "classpath:application-test.properties")
 @Slf4j
 abstract class TelegramTest {
-    private static final String NO_BOT_NAME_MARKER = "dummyBot";
-    private static final String NO_BOT_TOKEN_MARKER = "dummyToken";
-    static final String EMPTY_RESPONSE = "_void_";
 
-    @Value("${test.telegram.botname}")
-    private String botName;
+    boolean isLocalRun() {
+        String testUrl = System.getProperty(Selenide.Props.TEST_URL, "");
+        String dockerHost = "host.testcontainers.internal";
+        String localhost = "localhost";
 
-    @Value("${test.telegram.token}")
-    private String botToken;
-
-    String sendMessageToBot(String message) {
-        if (StringUtils.isBlank(message)) {
-            throw new IllegalArgumentException("You asked me send nothing to bot. Are you stupid ?");
-        }
-        if (!isCorrectlyInitialized()) {
-            log.warn("Cannot connect to Telegram with this bot name {} and/or token {}", botName, botToken);
-        }
-
-        String urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
-        urlString = String.format(urlString, botToken, botName, message);
-
-        String response = EMPTY_RESPONSE;
-        try {
-            URL url = new URL(urlString);
-            URLConnection conn = url.openConnection();
-            StringBuilder sb = new StringBuilder();
-            InputStream is = new BufferedInputStream(conn.getInputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String inputLine;
-            while ((inputLine = br.readLine()) != null) {
-                sb.append(inputLine);
-            }
-            response = sb.toString();
-        } catch (MalformedURLException e) {
-            log.error("Internal error: Malformed Telegram API URL", e);
-        } catch (IOException e) {
-            log.error("I/O error: Cannot open contact telegram bot. Do you have Internet access?", e);
-        }
-        return response;
-    }
-
-    boolean isCorrectlyInitialized() {
-        boolean allSet = true;
-        if (StringUtils.isBlank(botName) || botName.equals(NO_BOT_NAME_MARKER)) {
-            log.warn("Bot Name: not set. Do you have '-Dtelegram.botname=' in your run command ?");
-            allSet = false;
-        }
-        if (StringUtils.isBlank(botToken) || botToken.equals(NO_BOT_TOKEN_MARKER)) {
-            log.warn("Bot Name: not set. Do you have '-Dtelegram.token=' in your run command ?");
-            allSet = false;
-        }
-        return allSet;
+        return (testUrl.contains(dockerHost) || testUrl.contains(localhost));
     }
 
 }
