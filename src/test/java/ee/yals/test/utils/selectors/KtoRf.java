e9afd95a709fdae76c2d21e5dfd39b70926f080f
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2019-09-16 18:16:08 +0300

    Test written
    Signed-off-by: Alexander Muravya <alexander.muravya@kuehne-nagel.com>

---
+++ src/test/java/ee/yals/test/utils/selectors/KtoRf.java
@@ -0,0 +1,14 @@
+package ee.yals.test.utils.selectors;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+/**
+ * Set of constant values with CSS selectors from page http://кто.рф
+ *
+ * @since 2.5
+ */
+public class KtoRf {
+    public static final SelenideElement DIV_EGGS = $("div.eggs");
+}
