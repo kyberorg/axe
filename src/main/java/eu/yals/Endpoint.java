11edb43ed057b8df6e00029cd869f89ba71628ec
Alex Muravya <kyberorg@yadev.eu>
2019-09-27 21:19:41 +0300

    QR Code (#134)

--- src/main/java/eu/yals/Endpoint.java
+++ src/main/java/eu/yals/Endpoint.java
@@ -27,6 +27,11 @@ public class Endpoint {
     @RequestMethod(value = HttpMethod.GET, api = true)
     public static final String TELEGRAM_STATUS_API = "/api/tg/status";
 
+    @RequestMethod(value = HttpMethod.GET, api = true)
+    public static final String QR_CODE_API_BASE = "/api/qrCode/";
+    public static final String QR_CODE_API = QR_CODE_API_BASE + "{ident}";
+    public static final String CUSTOM_SIZE_QR_CODE_API = QR_CODE_API + "/" + "{size}";
+
     @RequestMethod(HttpMethod.GET)
     public static final String SLASH_BASE = "/";
     public static final String SLASH = "/{ident}";
