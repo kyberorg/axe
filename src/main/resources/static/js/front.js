f8e57a08d0b5e92e1f898d39528fbf5da0c4842e
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2019-10-02 17:24:35 +0300

    Changes in front.js

--- src/main/resources/static/js/front.js
+++ src/main/resources/static/js/front.js
@@ -1,6 +1,6 @@
 function cleanErrors() {
     $("#errorText").html("");
-    $("#errorModal").modal('hide');
+    $("#errorModal").modal("hide");
 }
 
 function cleanForm() {
@@ -9,88 +9,15 @@ function cleanForm() {
 
 function cleanResults() {
     $("#resultLink").html("").attr("href", "");
-    $("#result").addClass('invisible');
+    $("#result").addClass("invisible");
 
     $("#qrCode img").attr("src", "");
-    $("#qrCode").addClass('invisible');
+    $("#qrCode").addClass("invisible");
 }
 
 function showError(errorMessage) {
     $("#errorText").html(errorMessage);
-    $("#errorModal").modal('show');
-}
-
-function sendLink(long_url) {
-    var storeLinkRoute = "/api/store";
-
-    var longUrl = long_url.trim();
-    var json = {
-        link: longUrl
-    };
-    doPost(storeLinkRoute, json, onSuccessStoreLink, onFailStoreLink);
-}
-
-function onSuccessStoreLink(data, textStatus, jqXHR) {
-    cleanErrors();
-    cleanForm();
-    if (jqXHR.status === 201) {
-        var ident = data.ident;
-        if (ident === undefined || ident.trim().length === 0) {
-            showError("Internal error. Got malformed reply from server");
-            return;
-        }
-
-        $("#resultLink").html(window.location.origin + "/" + ident).attr("href", ident);
-        $("#result").removeClass('invisible');
-        updateCounter();
-        generateQRCode(ident);
-    }
-}
-
-function onFailStoreLink(jqXHR, textStatus, errorThrown) {
-    if (jqXHR !== null || jqXHR !== undefined) {
-
-        var replyRaw = jqXHR.responseText;
-        console.log("Reply JSON: " + replyRaw);
-        var reply = JSON.parse(replyRaw);
-
-        var errors = [];
-
-        if (reply.errors !== undefined) {
-            if (reply.errors.length > 0) {
-                $.each(reply.errors, function (index, value) {
-                    if (value.field === "link") {
-                        value.field = "Long URL";
-                    }
-                    errors.push(value.field + " " + value.errorMessage);
-                });
-            }
-        } else if (reply.error !== undefined) {
-            errors.push(reply.error.field + " " + reply.error.errorMessage);
-        }
-
-        var errorText = "Validation failed <BR>";
-        if (errors.length >= 0) {
-            errorText += "Errors: <BR>";
-            $.each(errors, function (index, value) {
-                errorText += value + "<BR>";
-            });
-        }
-        showError(errorText);
-    }
-}
-
-function copyLinkToClipboard() {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($("#resultLink").text()).select();
-    document.execCommand("copy");
-    $temp.remove();
-
-    $("#linkCopiedModal").modal('show');
-    setTimeout(function () {
-        $("#linkCopiedModal").modal('hide');
-    }, 1000);
+    $("#errorModal").modal("show");
 }
 
 function updateCounter() {
@@ -122,21 +49,8 @@ function calculateQRCodeSize() {
 
 function generateQRCode(ident) {
     var size = calculateQRCodeSize();
-    var qrCodeGeneratorRoute = "/api/qrCode/" + ident + "/" + parseInt(size);
+    var qrCodeGeneratorRoute = "/api/qrCode/" + ident + "/" + parseInt(size, 10);
     doGet(qrCodeGeneratorRoute, onSuccessGenerateQRCode, onFailGenerateQRCode);
-}
-
-function onSuccessGenerateQRCode(data, textStatus, jqXHR) {
-    if (jqXHR.status === 200) {
-        var qrCode = data.qrCode;
-        if (qrCode === undefined || qrCode.trim().length === 0) {
-            showError("Internal error. Got malformed reply from QR generator");
-            return;
-        }
-
-        $("#qrCode img").attr('src', qrCode);
-        $("#qrCode").removeClass('invisible');
-    }
 }
 
 function onFailGenerateQRCode(jqXHR, textStatus, errorThrown) {
@@ -152,6 +66,92 @@ function onFailGenerateQRCode(jqXHR, textStatus, errorThrown) {
     }
 }
 
+function onSuccessStoreLink(data, textStatus, jqXHR) {
+    cleanErrors();
+    cleanForm();
+    if (jqXHR.status === 201) {
+        var ident = data.ident;
+        if (ident === null || ident.trim().length === 0) {
+            showError("Internal error. Got malformed reply from server");
+            return;
+        }
+
+        $("#resultLink").html(window.location.origin + "/" + ident).attr("href", ident);
+        $("#result").removeClass("invisible");
+        updateCounter();
+        generateQRCode(ident);
+    }
+}
+
+function onSuccessGenerateQRCode(data, textStatus, jqXHR) {
+    if (jqXHR.status === 200) {
+        var qrCode = data.qrCode;
+        if (qrCode.trim().length === 0) {
+            showError("Internal error. Got malformed reply from QR generator");
+            return;
+        }
+
+        $("#qrCode img").attr("src", qrCode);
+        $("#qrCode").removeClass("invisible");
+    }
+}
+
+function onFailStoreLink(jqXHR, textStatus, errorThrown) {
+    if (jqXHR !== null) {
+
+        var replyRaw = jqXHR.responseText;
+        console.debug("Reply JSON: " + replyRaw);
+        var reply = JSON.parse(replyRaw);
+
+        var errors = [];
+
+        if (reply.errors !== undefined) {
+            if (reply.errors.length > 0) {
+                $.each(reply.errors, function (index, value) {
+                    if (value.field === "link") {
+                        value.field = "Long URL";
+                    }
+                    errors.push(value.field + " " + value.errorMessage);
+                });
+            }
+        } else if (reply.error !== undefined) {
+            errors.push(reply.error.field + " " + reply.error.errorMessage);
+        }
+
+        var errorText = "Validation failed <BR>";
+        if (errors.length >= 0) {
+            errorText += "Errors: <BR>";
+            $.each(errors, function (index, value) {
+                errorText += value + "<BR>";
+            });
+        }
+        showError(errorText);
+    }
+}
+
+function sendLink(longUrl) {
+    var storeLinkRoute = "/api/store";
+
+    longUrl = longUrl.trim();
+    var json = {
+        link: longUrl
+    };
+    doPost(storeLinkRoute, json, onSuccessStoreLink, onFailStoreLink);
+}
+
+function copyLinkToClipboard() {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($("#resultLink").text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+
+    $("#linkCopiedModal").modal("show");
+    setTimeout(function () {
+        $("#linkCopiedModal").modal("hide");
+    }, 1000);
+}
+
 function handleForm(e) {
     e.preventDefault();
     cleanErrors();
@@ -160,9 +160,9 @@ function handleForm(e) {
     var isFormValid = true;
 
     var longUrl = $("#longUrl").val();
-    console.log("Got long URL: " + longUrl);
+    console.debug("Got long URL: " + longUrl);
     cleanForm();
-    if (longUrl === undefined || longUrl.trim().length === 0) {
+    if (longUrl.trim().length === 0) {
         var errorMessage = "Long URL cannot be empty";
         showError(errorMessage);
         isFormValid = false;
@@ -173,7 +173,7 @@ function handleForm(e) {
 }
 
 $(document).ready(function () {
-    $("#shortenIt").on('click', handleForm);
+    $("#shortenIt").on("click", handleForm);
     $('[data-toggle="tooltip"]').tooltip();
-    $("#copyLink").on('click', copyLinkToClipboard);
+    $("#copyLink").on("click", copyLinkToClipboard);
 });
