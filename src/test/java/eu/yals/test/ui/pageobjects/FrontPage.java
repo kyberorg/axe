ff341ba737ec45c12125a493ef011af9ae3916eb
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2019-11-13 09:51:21 +0200

    Tests: selectors

--- src/test/java/eu/yals/test/ui/pageobjects/FrontPage.java
+++ src/test/java/eu/yals/test/ui/pageobjects/FrontPage.java
@@ -17,12 +17,12 @@ public class FrontPage {
     }
 
     public static class MainRow {
-        public static final SelenideElement MAIN_DIV = $("#main");
-        public static final SelenideElement H2 = $("#main h2");
+        public static final SelenideElement MAIN_DIV = $("#mainArea");
+        public static final SelenideElement H2 = $("#mainArea h2");
         public static final SelenideElement FORM = $("form");
         public static final String INPUT_ID = "#longUrl";
-        public static final SelenideElement LONG_URL_INPUT = $("#longUrl");
-        public static final SelenideElement SUBMIT_BUTTON = $("#shortenIt");
+        public static final SelenideElement LONG_URL_INPUT = $("#mainArea vaadin-text-field input");
+        public static final SelenideElement SUBMIT_BUTTON = $("#mainArea vaadin-button button");
         public static final SelenideElement PUBLIC_ACCESS_BANNER = $("#publicAccessBanner");
 
         public static final SelenideElement HW = $("#home h1");
