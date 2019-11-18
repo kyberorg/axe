7403bd39f99a16afdd733b0b510c626e7d8fa263
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2019-11-18 21:24:00 +0100

    Tests: Raw Page Object

--- src/test/java/eu/yals/test/ui/vaadin/PocIT.java
+++ src/test/java/eu/yals/test/ui/vaadin/PocIT.java
@@ -1,44 +1,23 @@
 package eu.yals.test.ui.vaadin;
 
-import com.vaadin.flow.component.html.testbench.H2Element;
-import com.vaadin.testbench.annotations.BrowserConfiguration;
 import com.vaadin.testbench.annotations.RunOnHub;
-import com.vaadin.testbench.parallel.BrowserUtil;
-import com.vaadin.testbench.parallel.ParallelTest;
-import eu.yals.test.TestApp;
+import eu.yals.test.ui.vaadin.pageobjects.HomeViewPage;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.ArrayList;
-import java.util.List;
+@RunOnHub("ci.yadev.eu") //TODO customize
+public class PocIT extends VaadinTest<HomeViewPage> {
 
-//@RunOnHub
-@RunOnHub("ci.yadev.eu")
-public class PocIT extends ParallelTest {
-
-    private final static int SERVER_PORT = Integer.parseInt(System.getProperty(TestApp.Properties.SERVER_PORT, "8080"));
-    private final static String LOCAL_URL = String.format("http://host.testcontainers.internal:%d", SERVER_PORT);
-    protected final static String BASE_URL = System.getProperty(TestApp.Properties.TEST_URL, LOCAL_URL);
-
-    @Before
-    public void setup() throws Exception {
-        super.setup();
-        getDriver().get(BASE_URL);
-    }
-
-    @BrowserConfiguration
-    public List<DesiredCapabilities> getBrowserConfiguration() {
-        List<DesiredCapabilities> browsers = new ArrayList<>();
-        browsers.add(BrowserUtil.chrome());
-        browsers.add(BrowserUtil.firefox());
-        return browsers;
+    @Override
+    protected HomeViewPage openView() {
+        return $(HomeViewPage.class).waitForFirst();
     }
 
     @Test
     public void testVaadin() {
-        String title = $(H2Element.class).first().getText();
+        HomeViewPage homeView = $(HomeViewPage.class).first();
+
+        String title = homeView.title.getText();
         Assert.assertEquals("Yet another link shortener", title);
     }
 }
