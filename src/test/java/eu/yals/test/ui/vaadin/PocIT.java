13c2d01e5e36d24ba8701b31cd8c90da7a205a6f
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2019-11-20 00:11:49 +0100

    Tests: Finally adding Testing name

--- src/test/java/eu/yals/test/ui/vaadin/PocIT.java
+++ src/test/java/eu/yals/test/ui/vaadin/PocIT.java
@@ -1,10 +1,8 @@
 package eu.yals.test.ui.vaadin;
 
-import com.vaadin.testbench.parallel.TestNameSuffix;
 import eu.yals.test.ui.vaadin.pageobjects.HomeViewElement;
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.Cookie;
 
 public class PocIT extends VaadinTest<HomeViewElement> {
     @Override
@@ -12,22 +10,12 @@ public class PocIT extends VaadinTest<HomeViewElement> {
         return $(HomeViewElement.class).waitForFirst();
     }
 
-    @TestNameSuffix(property = "MySuperTest")
     @Test
     public void testVaadin() {
         HomeViewElement homeView = openView();
 
-        Cookie cookie = new Cookie("zaleniumMessage", "Checking Title");
-        getDriver().manage().addCookie(cookie);
-
         String titleText = homeView.getTitleField().getText();
         Assert.assertEquals("Yet another link shortener", titleText);
 
-        //TODO move to TestWatcher
-        Cookie cookie2 = new Cookie("zaleniumTestPassed", "true");
-        getDriver().manage().addCookie(cookie2);
-
-        Cookie cookie3 = new Cookie("zaleniumTestName", "true");
-        getDriver().manage().addCookie(cookie3);
     }
 }
