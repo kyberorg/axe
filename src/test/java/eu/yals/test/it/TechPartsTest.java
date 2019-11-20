2a2d0089cef1a86701713ec902de9052bde0077a
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2019-11-20 15:28:30 +0100

    Tests: TechParts moved from Mock to Unirest

--- src/test/java/eu/yals/test/it/TechPartsTest.java
+++ src/test/java/eu/yals/test/it/TechPartsTest.java
@@ -1,101 +1,57 @@
 package eu.yals.test.it;
 
+import com.mashape.unirest.http.HttpResponse;
 import eu.yals.Endpoint;
-import eu.yals.constants.Header;
 import eu.yals.constants.MimeType;
 import eu.yals.test.TestUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.junit.Before;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.shaded.org.apache.commons.lang.StringUtils;
 
-import static org.junit.Assert.assertNotNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-/**
- * Robots, humans and favicon
- *
- * @since 1.0
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath*:test-app.xml"})
-@WebAppConfiguration
-@TestPropertySource(locations = "classpath:application-test.properties")
+@Slf4j
 public class TechPartsTest {
 
-    @Autowired
-    private WebApplicationContext wac;
-
-    private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-    }
-
     @Test
-    public void robotsTxtIsPresentAndText() throws Exception {
-        assertNotNull(this.mockMvc);
-        MvcResult result = mockMvc.perform(get(Endpoint.ROBOTS_TXT)
-                .header(Header.TEST, ""))
-                .andExpect(status().is(200))
-                .andReturn();
+    public void robotsTxtIsPresentAndText() {
+        String robotsTxt = String.format("%s%s", TestUtils.getTestUrl(), Endpoint.ROBOTS_TXT);
+        HttpResponse<String> response = TestUtils.unirestGet(robotsTxt);
+        log.debug("Response: {}", response);
+        if (response == null) return;
 
-        result = goForwardIsForwardUrlPerformExists(result);
+        Assert.assertEquals(200, response.getStatus());
 
-        TestUtils.assertContentNotEmpty("robots.txt are empty", result);
-        TestUtils.assertContentType(MimeType.TEXT_PLAIN, result);
+        String body = response.getBody();
+        Assert.assertTrue("robots.txt is empty", StringUtils.isNotBlank(body));
+        TestUtils.assertContentType(MimeType.TEXT_PLAIN, response);
     }
 
     @Test
     public void humansTxtIsPresentAndText() throws Exception {
-        assertNotNull(this.mockMvc);
-        MvcResult result = mockMvc.perform(get(Endpoint.HUMANS_TXT)
-                .header(Header.TEST, ""))
-                .andExpect(status().is(200))
-                .andReturn();
+        String humansTxt = String.format("%s%s", TestUtils.getTestUrl(), Endpoint.HUMANS_TXT);
+        HttpResponse<String> response = TestUtils.unirestGet(humansTxt);
+        log.debug("Response: {}", response);
+        if (response == null) return;
 
-        result = goForwardIsForwardUrlPerformExists(result);
+        Assert.assertEquals(200, response.getStatus());
 
-        TestUtils.assertContentNotEmpty("humans.txt are empty", result);
-        TestUtils.assertContentType(MimeType.TEXT_PLAIN, result);
+        String body = response.getBody();
+        Assert.assertTrue("humans.txt is empty", StringUtils.isNotBlank(body));
+        TestUtils.assertContentType(MimeType.TEXT_PLAIN, response);
     }
 
     @Test
     public void faviconIsPresentAndIcon() throws Exception {
-        assertNotNull(this.mockMvc);
-        MvcResult result = mockMvc.perform(get(Endpoint.FAVICON_ICO)
-                .header(Header.TEST, ""))
-                .andExpect(status().is(200))
-                .andReturn();
+        String favIcon = String.format("%s%s", TestUtils.getTestUrl(), Endpoint.FAVICON_ICO);
+        HttpResponse<String> response = TestUtils.unirestGet(favIcon);
+        log.debug("Response: {}", response);
+        if (response == null) return;
 
-        result = goForwardIsForwardUrlPerformExists(result);
+        Assert.assertEquals(200, response.getStatus());
 
-        TestUtils.assertContentNotEmpty(result);
+        String body = response.getBody();
+        Assert.assertTrue("favicon.ico is empty", StringUtils.isNotBlank(body));
         //in Spring boot 2 favicon has image/x-icon mimetype
-        TestUtils.assertContentType(MimeType.IMAGE_X_ICON, result);
-    }
-
-    private MvcResult goForwardIsForwardUrlPerformExists(MvcResult result) throws Exception {
-        String forwardedUrl = result.getResponse().getForwardedUrl();
-        if (StringUtils.isNotBlank(forwardedUrl)) {
-            return mockMvc.perform(get(forwardedUrl))
-                    .andExpect(status().is(200))
-                    .andReturn();
-        } else {
-            return result;
-        }
+        TestUtils.assertContentType(MimeType.IMAGE_X_ICON, response);
     }
 }
