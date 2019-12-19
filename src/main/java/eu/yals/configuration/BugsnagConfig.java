ad21ff54312d896820776cac25d43cc0413eecfd
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2019-12-19 12:36:01 +0200

    bugsnag reporting in right way.

--- src/main/java/eu/yals/configuration/BugsnagConfig.java
+++ src/main/java/eu/yals/configuration/BugsnagConfig.java
@@ -3,6 +3,7 @@ package eu.yals.configuration;
 import com.bugsnag.Bugsnag;
 import com.bugsnag.BugsnagSpringConfiguration;
 import eu.yals.constants.App;
+import eu.yals.utils.git.MavenGitInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
@@ -21,9 +22,11 @@ public class BugsnagConfig {
     private static final String NO_TOKEN = "noToken";
 
     private final Environment env;
+    private MavenGitInfo mavenGitInfo;
 
-    public BugsnagConfig(Environment env) {
+    public BugsnagConfig(Environment env, MavenGitInfo gitInfo) {
         this.env = env;
+        this.mavenGitInfo = gitInfo;
     }
 
     private String proxyHost;
@@ -38,6 +41,11 @@ public class BugsnagConfig {
         if (hasProxy()) {
             setProxy();
         }
+        bugsnag.setAppType("maven");
+        if (mavenGitInfo.isApplicable()) {
+            bugsnag.setAppVersion(mavenGitInfo.getLatestTag());
+        }
+        setApplicationStage();
         return bugsnag;
     }
 
@@ -50,5 +58,10 @@ public class BugsnagConfig {
 
     private void setProxy() {
         bugsnag.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, Integer.parseInt(proxyPort))));
+    }
+
+    private void setApplicationStage() {
+        String stage = env.getProperty(App.Properties.APPLICATION_STAGE, "unknown");
+        bugsnag.setReleaseStage(stage);
     }
 }
