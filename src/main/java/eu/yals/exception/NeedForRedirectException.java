e0524d70381b04f3280ba70f8bc1950fc7358c9d
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2019-12-23 14:13:11 +0200

    fix for Vene Wiki

---
+++ src/main/java/eu/yals/exception/NeedForRedirectException.java
@@ -0,0 +1,7 @@
+package eu.yals.exception;
+
+/**
+ * Exception, which not intended to be thrown as exception, just {@link eu.yals.ui.special.RedirectToLinkView} ui handles it
+ */
+public class NeedForRedirectException extends YalsException {
+}
--- src/main/java/eu/yals/ui/special/RedirectToLinkView.java
+++ src/main/java/eu/yals/ui/special/RedirectToLinkView.java
@@ -11,19 +11,32 @@ import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import eu.yals.Endpoint;
 import eu.yals.constants.Header;
+import eu.yals.exception.NeedForRedirectException;
+import eu.yals.utils.AppUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
+@Slf4j
 @SpringComponent
 @UIScope
 @Route(Endpoint.TNT.REDIRECTOR)
-public class RedirectToLinkView extends VerticalLayout implements HasErrorParameter<ArithmeticException> {
+public class RedirectToLinkView extends VerticalLayout implements HasErrorParameter<NeedForRedirectException> {
+    private static final String TAG = "[" + RedirectToLinkView.class.getSimpleName() + "]";
 
     public RedirectToLinkView() {
         add(new Text("Not intended for direct use. Needs parameter"));
     }
 
     @Override
-    public int setErrorParameter(BeforeEnterEvent event, ErrorParameter<ArithmeticException> parameter) {
-        VaadinResponse.getCurrent().setHeader(Header.LOCATION, parameter.getCustomMessage());
-        return 302;
+    public int setErrorParameter(BeforeEnterEvent event, ErrorParameter<NeedForRedirectException> parameter) {
+        String target = parameter.getCustomMessage();
+        if (StringUtils.isNotBlank(target)) {
+            VaadinResponse.getCurrent().setHeader(Header.LOCATION, AppUtils.covertUnicodeToAscii(target));
+            return 302;
+        } else {
+            log.error("{} Target is empty", TAG);
+            return 500;
+        }
+
     }
 }
