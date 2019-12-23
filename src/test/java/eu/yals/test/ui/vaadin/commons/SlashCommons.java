e847384821f0e0b5e9c3bdcce7b383172df66690
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2019-12-23 14:21:42 +0200

    tests

--- src/test/java/eu/yals/test/ui/vaadin/commons/SlashCommons.java
+++ src/test/java/eu/yals/test/ui/vaadin/commons/SlashCommons.java
@@ -1,6 +1,8 @@
 package eu.yals.test.ui.vaadin.commons;
 
 import eu.yals.test.ui.vaadin.pageobjects.HomeViewElement;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class SlashCommons extends VaadinTest<HomeViewElement> {
     @Override
@@ -8,9 +10,18 @@ public class SlashCommons extends VaadinTest<HomeViewElement> {
         return $(HomeViewElement.class).waitForFirst();
     }
 
+    /**
+     * Otherwise error (No runnable methods) given
+     */
+    @Test
+    public void dummyTest() {
+        Assert.assertTrue(true);
+    }
+
     protected void pasteValueInFormAndSubmitIt(String link) {
         HomeViewElement homeView = openView();
         homeView.getInputField().setValue(link);
         homeView.getSubmitButton().click();
     }
+
 }
