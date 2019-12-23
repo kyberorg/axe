015cfd5b2d2de7095d0b37cd396c3aaeeced61a7
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2019-12-23 11:00:11 +0200

    UI actions

--- src/main/java/eu/yals/ui/HomeView.java
+++ src/main/java/eu/yals/ui/HomeView.java
@@ -2,6 +2,7 @@ package eu.yals.ui;
 
 import com.github.appreciated.app.layout.annotations.Caption;
 import com.github.appreciated.app.layout.annotations.Icon;
+import com.vaadin.flow.component.ClickEvent;
 import com.vaadin.flow.component.board.Board;
 import com.vaadin.flow.component.board.Row;
 import com.vaadin.flow.component.button.Button;
@@ -18,10 +19,14 @@ import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import eu.yals.Endpoint;
 import eu.yals.constants.App;
+import eu.yals.json.StoreRequestJson;
 import eu.yals.services.GitService;
 import eu.yals.services.overall.OverallService;
 import eu.yals.ui.css.HomeViewCss;
 import eu.yals.utils.AppUtils;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
@@ -44,16 +49,23 @@ public class HomeView extends VerticalLayout {
     private final HomeViewCss homeViewCss;
     private final OverallService overallService;
     private final GitService gitService;
+    private final AppUtils appUtils;
+
+    private TextField input;
+    Anchor shortLink;
+    Image qrCode;
 
     private String latestCommit;
     private String latestTag;
 
     private Span linkCounter;
 
-    public HomeView(HomeViewCss css, OverallService overallService, GitService gitService) {
+
+    public HomeView(HomeViewCss css, OverallService overallService, GitService gitService, AppUtils appUtils) {
         this.homeViewCss = css;
         this.overallService = overallService;
         this.gitService = gitService;
+        this.appUtils = appUtils;
 
         init();
         applyStyle();
@@ -138,7 +150,8 @@ public class HomeView extends VerticalLayout {
         subtitle.setId(IDs.SUBTITLE);
         homeViewCss.makeSubtitleItalic(subtitle);
 
-        TextField input = new TextField("Your very long URL here:");
+
+        input = new TextField("Your very long URL here:");
         input.setId(IDs.INPUT);
         input.setPlaceholder("http://mysuperlongurlhere.tld");
         input.setWidthFull();
@@ -149,6 +162,7 @@ public class HomeView extends VerticalLayout {
         Button submitButton = new Button("Shorten it!");
         submitButton.setId(IDs.SUBMIT_BUTTON);
         submitButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        submitButton.addClickListener(this::onSaveLink);
 
         VerticalLayout mainArea = new VerticalLayout(title, subtitle, input, publicAccessBanner, submitButton);
         mainArea.setId(IDs.MAIN_AREA);
@@ -178,12 +192,13 @@ public class HomeView extends VerticalLayout {
 
         Span emptySpan = new Span();
 
-        Anchor shortLink = new Anchor("https://yals.eu/gMKyrJ", "https://yals.eu/gMKyrJ");
+        shortLink = new Anchor("https://yals.eu/gMKyrJ", "https://yals.eu/gMKyrJ");
         shortLink.setId(IDs.SHORT_LINK);
         homeViewCss.makeLinkStrong(shortLink);
 
         Button copyLinkButton = new Button(VaadinIcon.PASTE.create());
         copyLinkButton.setId(IDs.COPY_LINK_BUTTON);
+        copyLinkButton.addClickListener(this::copyLinkToClipboard);
 
         homeViewCss.applyResultAreaStyle(resultArea);
         resultArea.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
@@ -195,7 +210,8 @@ public class HomeView extends VerticalLayout {
     private Div qrCodeArea() {
         Div qrCodeArea = new Div();
         qrCodeArea.setId(IDs.QR_CODE_AREA);
-        Image qrCode = new Image();
+
+        qrCode = new Image();
         qrCode.setId(IDs.QR_CODE);
         qrCode.setSrc("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAV4AAAFeAQAAAADlUEq3AAABVklEQVR42u3aSw6DIBCAYRIP4JG8ukfyACRTZR4Qa9qumiH5WZiin6spAwMW+b3VAgaDwWAwGJwQ78Wa3Tvsl7UVnB737rFIwyd5eApOi6s9vR6UTdrl6l5/AfBMuIW7jV8wGPxP3LtVZ8mf8jM4DY5lz6551y5f1kjgNLg3nTSPPkw/1SngNFgza4R2L4uMlQh4FtyqjsWzrVaQzykXnAxr85LR58saXXB23CI9ZFsfunrZBJwer73MOOO7xS7OpuMXnB1fze4psZhbLQlOj+/vHubaHsCHbXBwFixRa8SAPU2kYQFnx+1e0Y03j75tCSzvxSM4H76V/7HisTJyBWfH4xacB15PpsCT4OhqEeLZFjwP7sfBdTiFaikXPBUW8QP94kP3fYYFJ8a7nwnrHoAtZsH58ZhyLdyefJ9KD3A2fD8OFhlfW8HZMd8/g8FgMBgMnhC/AP4haj2FcNJGAAAAAElFTkSuQmCC");
         qrCode.setAlt("qrCode");
@@ -226,6 +242,140 @@ public class HomeView extends VerticalLayout {
         footer.add(version);
 
         return footer;
+    }
+
+    private void onSaveLink(ClickEvent<Button> buttonClickEvent) {
+        cleanErrors();
+        cleanResults();
+
+        boolean isFormValid = true;
+        String longUrl = input.getValue();
+        log.debug("{} Got long URL: {}", TAG, longUrl);
+        cleanForm();
+
+        if (StringUtils.isBlank(longUrl)) {
+            String errorMessage = "Long URL cannot be empty";
+            showError(errorMessage);
+            isFormValid = false;
+        }
+        if (isFormValid) {
+            sendLink(longUrl);
+        }
+    }
+
+    private void copyLinkToClipboard(ClickEvent<Button> buttonClickEvent) {
+         //TODO https://vaadin.com/directory/component/v-clipboard/samples
+    }
+
+    private void sendLink(String link) {
+        final String apiRoute = Endpoint.Api.STORE_API;
+        StoreRequestJson json = StoreRequestJson.create().withLink(link);
+        HttpResponse<JsonNode> response = Unirest.post(appUtils.getAPIHostPort() + apiRoute).body(json).asJson();
+        if (response.isSuccess()) {
+            onSuccessStoreLink(response);
+        } else {
+            onFailStoreLink(response);
+        }
+    }
+
+    private void onSuccessStoreLink(HttpResponse<JsonNode> response) {
+        cleanErrors();
+        cleanForm();
+        if (response.getStatus() == 201) {
+            JsonNode json = response.getBody();
+            String ident = json.getObject().getString("ident");
+            if (StringUtils.isNotBlank(ident)) {
+                shortLink.setTarget(appUtils.getServerUrl() + "/" + ident);
+                resultRow.setVisible(true);
+                updateCounter();
+                generateQRCode(ident);
+            } else {
+                showError("Internal error. Got malformed reply from server");
+            }
+        }
+    }
+
+    private void onFailStoreLink(HttpResponse<JsonNode> response) {
+        JsonNode json = response.getBody();
+        log.error("{} Failed to store link. Reply: {}", TAG, json);
+        String message = json.getObject().getString("message");
+        //TODO validation
+        showError(message);
+    }
+
+    private void updateCounter() {
+        int currentNumber = Integer.parseInt(linkCounter.getText());
+        linkCounter.setText(String.valueOf(currentNumber + 1));
+    }
+
+    private int calculateQRCodeSize() {
+        double browserWidth = Double.parseDouble(getWidth());
+        int defaultQRBlockSize = 371;
+        int defaultQRCodeSize = 350;
+        double qrBlockRatio = 0.943; //350/371
+
+        int size;
+        if (browserWidth > defaultQRBlockSize) {
+            size = defaultQRCodeSize;
+        } else {
+            size = (int) (browserWidth * qrBlockRatio);
+        }
+        return size;
+    }
+
+    private void generateQRCode(String ident) {
+        int size = calculateQRCodeSize();
+        String qrCodeGeneratorRoute = String.format("%s/%s/%s/%d", appUtils.getServerUrl(), Endpoint.Api.QR_CODE_API,
+                ident, size);
+        HttpResponse<JsonNode> response = Unirest.get(qrCodeGeneratorRoute).asJson();
+        if (response.isSuccess()) {
+            onSuccessGenerateQRCode(response);
+        } else {
+            onFailGenerateQRCode(response);
+        }
+    }
+
+    private void onSuccessGenerateQRCode(HttpResponse<JsonNode> response) {
+        if (response.getStatus() == 200) {
+            String qrCode = response.getBody().getObject().getString("qrCode");
+            if (StringUtils.isNotBlank(qrCode)) {
+                this.qrCode.setSrc(qrCode);
+                qrCodeRow.setVisible(true);
+            } else {
+                showError("Internal error. Got malformed reply from QR generator");
+            }
+        }
+    }
+
+    private void onFailGenerateQRCode(HttpResponse<JsonNode> response) {
+        showError("Internal error. Got malformed reply from QR generator");
+        this.qrCode.setSrc("");
+        qrCodeRow.setVisible(false);
+
+        if (response.getBody() != null) {
+            log.debug("{} QR Code Reply JSON: {}", TAG, response.getBody());
+        }
+    }
+
+    private void showError(String errorMessage) {
+        //TODO errorModal
+    }
+
+    private void cleanForm() {
+        input.setValue("");
+    }
+
+    private void cleanErrors() {
+        //TODO errorModal
+    }
+
+    private void cleanResults() {
+        shortLink.setHref("");
+        shortLink.setTarget("");
+        resultRow.setVisible(false);
+
+        qrCode.setSrc("");
+        qrCodeRow.setVisible(false);
     }
 
     public static class IDs {
