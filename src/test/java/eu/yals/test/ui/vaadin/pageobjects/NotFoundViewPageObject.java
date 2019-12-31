8b0135c4582de1d18b5b73b3749fe7236652beaf
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2019-12-31 09:34:47 +0200

    Vaadin TB: Page Object tööta raisk!

--- src/test/java/eu/yals/test/ui/vaadin/pageobjects/NotFoundViewPageObject.java
+++ src/test/java/eu/yals/test/ui/vaadin/pageobjects/NotFoundViewPageObject.java
@@ -6,8 +6,11 @@ import com.vaadin.flow.component.html.testbench.H1Element;
 import com.vaadin.flow.component.html.testbench.ImageElement;
 import com.vaadin.flow.component.html.testbench.SpanElement;
 import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.annotations.Attribute;
 import eu.yals.test.ui.vaadin.tech.ElementConverter;
+import eu.yals.ui.err.NotFoundView;
 
+@Attribute(name = "id", value = NotFoundView.IDs.VIEW_ID)
 public class NotFoundViewPageObject extends TestBenchElement {
 
     public final SelenideElement TITLE = ElementConverter.get().convert(getTitle());
