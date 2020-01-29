fcd9234df35f0972806e9c4e7bbb2ec8a7476195
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2020-01-29 12:30:50 +0200

    Vaadin TB only

--- src/test/java/eu/yals/test/ui/vaadin/SlashIT.java
+++ src/test/java/eu/yals/test/ui/vaadin/SlashIT.java
@@ -63,8 +63,8 @@ public class SlashIT extends VaadinTest<HomeViewElement> {
 
     private void verifyThatPage404Opened() {
         NotFoundViewPageObject page404 = NotFoundViewPageObject.getPageObject(getDriver());
-
-        selenideElement(page404.getTitle()).should(exist);
+        Assert.assertTrue(page404.getTitle().isDisplayed());
+        //selenideElement(page404.getTitle()).should(exist);
         Assert.assertTrue(page404.getTitleText().contains("404"));
     }
 }
