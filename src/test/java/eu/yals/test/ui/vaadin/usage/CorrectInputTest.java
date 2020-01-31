329a1c8599cdab69faa4c3155c6b2be8d78172e6
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2020-01-31 09:59:02 +0200

    Not Before

--- src/test/java/eu/yals/test/ui/vaadin/usage/CorrectInputTest.java
+++ src/test/java/eu/yals/test/ui/vaadin/usage/CorrectInputTest.java
@@ -2,7 +2,6 @@ package eu.yals.test.ui.vaadin.usage;
 
 import eu.yals.test.ui.vaadin.VaadinTest;
 import eu.yals.test.ui.vaadin.pageobjects.HomeViewPageObject;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -16,7 +15,6 @@ import static org.junit.Assert.assertTrue;
 public class CorrectInputTest extends VaadinTest {
   private HomeViewPageObject homeView;
 
-  @Before
   public void openUrl() {
     open("/");
     homeView = HomeViewPageObject.getPageObject(getDriver());
@@ -24,6 +22,7 @@ public class CorrectInputTest extends VaadinTest {
 
   @Test
   public void httpLink() {
+    openUrl();
     String link = "http://virtadev.net";
     homeView.pasteValueInFormAndSubmitIt(link);
     checkExpectedBehavior();
@@ -31,6 +30,7 @@ public class CorrectInputTest extends VaadinTest {
 
   @Test
   public void httpsLink() {
+    openUrl();
     String link = "https://github.com/virtalab";
     homeView.pasteValueInFormAndSubmitIt(link);
     checkExpectedBehavior();
@@ -38,6 +38,7 @@ public class CorrectInputTest extends VaadinTest {
 
   @Test
   public void ftpLink() {
+    openUrl();
     String link = "ftp://ftp.yandex.ru";
     homeView.pasteValueInFormAndSubmitIt(link);
     checkExpectedBehavior();
@@ -45,6 +46,7 @@ public class CorrectInputTest extends VaadinTest {
 
   @Test
   public void cyrillicLink() {
+    openUrl();
     String link = "http://президент.рф";
     homeView.pasteValueInFormAndSubmitIt(link);
     checkExpectedBehavior();
@@ -52,6 +54,7 @@ public class CorrectInputTest extends VaadinTest {
 
   @Test
   public void linkWithoutProtocol() {
+    openUrl();
     String link = "www.kv.ee/2992207";
     homeView.pasteValueInFormAndSubmitIt(link);
     checkExpectedBehavior();
@@ -59,6 +62,7 @@ public class CorrectInputTest extends VaadinTest {
 
   @Test
   public void linkFromRussianWikipedia() {
+    openUrl();
     String link =
         "https://ru.wikipedia.org/wiki/%D0%94%D0%B5%D0%BF%D0%BE%D1%80%D1%82%D0%B0%D1%86%D0%B8%D0%B8_%D0%B8%D0%B7_%D0%AD%D1%81%D1%82%D0%BE%D0%BD%D1%81%D0%BA%D0%BE%D0%B9_%D0%A1%D0%BE%D0%B2%D0%B5%D1%82%D1%81%D0%BA%D0%BE%D0%B9_%D0%A1%D0%BE%D1%86%D0%B8%D0%B0%D0%BB%D0%B8%D1%81%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B9_%D0%A0%D0%B5%D1%81%D0%BF%D1%83%D0%B1%D0%BB%D0%B8%D0%BA%D0%B8";
     homeView.pasteValueInFormAndSubmitIt(link);
