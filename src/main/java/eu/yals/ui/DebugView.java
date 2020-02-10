1312eec061ba61ab32d07c6c26896ecc4183f652
Alexander Muravya <alexander.muravya@kuehne-nagel.com>
2020-02-10 16:21:29 +0200

    More debug

--- src/main/java/eu/yals/ui/DebugView.java
+++ src/main/java/eu/yals/ui/DebugView.java
@@ -6,7 +6,9 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.SpringComponent;
@@ -29,6 +31,8 @@ public class DebugView extends Div {
     button.addClickListener(event -> span.setText("Button clicked"));
     ClipboardHelper clipboardHelper = new ClipboardHelper("some stuff", button);
     TextField input = new TextField("Insert some stuff here");
+    input.setValueChangeMode(ValueChangeMode.ON_CHANGE);
+    input.addValueChangeListener(event -> Notification.show(event.getValue()));
 
     add(clipboardHelper, input, span);
   }
