57bb706dd2955e2a29a60aad4dbbc91a07c2a465
alexander.muravya <kyberorg@yadev.eu>
2020-04-20 11:01:35 +0300

    #142 AppInfoViewTest

--- src/test/java/eu/yals/test/ui/appinfo/PublicInfoTestIT.java
+++ src/test/java/eu/yals/test/ui/appinfo/PublicInfoTestIT.java
@@ -1,9 +1,7 @@
 package eu.yals.test.ui.appinfo;
 
-import eu.yals.services.GitService;
 import eu.yals.test.ui.AppInfoPageTest;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,28 +12,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class PublicInfoTestIT extends AppInfoPageTest {
 
-    @Autowired
-    private GitService gitService;
-
     @Test
     public void publicAreaHasAllRequiredElements() {
         openPage();
-        if (gitService.tagPresent() && gitService.commitPresent()) {
-            //no git info aka local run
-            return;
-        }
 
-        if (gitService.tagPresent()) {
-            $$(appInfoView.getVersion()).shouldBeDisplayed();
-            $$(appInfoView.getVersion()).shouldHaveText("version");
-            $$(appInfoView.getVersion()).shouldHaveText("commit");
+        $$(appInfoView.getVersion()).shouldBeDisplayed();
+        $$(appInfoView.getVersion()).shouldHaveText("version");
+        $$(appInfoView.getVersion()).shouldHaveText("commit");
 
-            if (gitService.commitPresent()) {
-                $$(appInfoView.getCommitLink()).shouldBeDisplayed();
-                $$(appInfoView.getCommitLink()).shouldNotBeEmpty();
-                $$(appInfoView.getCommitLink()).shouldHaveAttr("href");
-            }
-        }
+        $$(appInfoView.getCommitLink()).shouldBeDisplayed();
+        $$(appInfoView.getCommitLink()).shouldNotBeEmpty();
+        $$(appInfoView.getCommitLink()).shouldHaveAttr("href");
     }
 
 }
