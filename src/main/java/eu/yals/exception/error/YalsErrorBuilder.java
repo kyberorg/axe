e59426bb9cb00e8427b6aa4ed582f731bc73f9d8
alexander.muravya <kyberorg@yadev.eu>
2020-04-22 15:16:29 +0300

    #162 core and exceptions

--- src/main/java/eu/yals/exception/error/YalsErrorBuilder.java
+++ src/main/java/eu/yals/exception/error/YalsErrorBuilder.java
@@ -2,41 +2,72 @@ package eu.yals.exception.error;
 
 import org.apache.commons.lang3.StringUtils;
 
+import static eu.yals.constants.HttpCode.STATUS_500;
+
 /**
- * Builder for {@link YalsError}
+ * Builder for {@link YalsError}.
  *
  * @since 2.7
  */
 @SuppressWarnings("UnusedReturnValue") //normal for Builder
-public class YalsErrorBuilder {
+public final class YalsErrorBuilder {
     private String messageToUser;
     private final String techMessage;
-    private int httpStatus = 500;
+    private int httpStatus = STATUS_500;
     private Throwable rawException = null;
 
-    public static YalsErrorBuilder withTechMessage(String techMessage) {
+    /**
+     * Adds technical message to {@link YalsError}. Intended as {@link YalsErrorBuilder} start point.
+     *
+     * @param techMessage not empty string with tech message
+     * @return {@link YalsErrorBuilder} object to continue building {@link YalsError}
+     */
+    public static YalsErrorBuilder withTechMessage(final String techMessage) {
         return new YalsErrorBuilder(techMessage);
     }
-    
-    private YalsErrorBuilder(String techMessage) {
+
+    private YalsErrorBuilder(final String techMessage) {
         this.techMessage = techMessage;
     }
 
-    public YalsErrorBuilder withMessageToUser(String messageToUser) {
+    /**
+     * Adds user-friendly message.
+     *
+     * @param messageToUser non-empty string with message to user.
+     * @return {@link YalsErrorBuilder} object to continue building {@link YalsError}
+     */
+    public YalsErrorBuilder withMessageToUser(final String messageToUser) {
         this.messageToUser = messageToUser;
         return this;
     }
 
-    public YalsErrorBuilder withStatus(int status) {
+    /**
+     * Adds http status.
+     *
+     * @param status int with valid {@link eu.yals.constants.HttpCode}
+     * @return {@link YalsErrorBuilder} object to continue building {@link YalsError}
+     */
+    public YalsErrorBuilder withStatus(final int status) {
         this.httpStatus = status;
         return this;
     }
 
-    public YalsErrorBuilder addRawException(Throwable throwable) {
+    /**
+     * Adds exception object.
+     *
+     * @param throwable {@link Throwable} object with exception caused issue.
+     * @return {@link YalsErrorBuilder} object to continue building {@link YalsError}
+     */
+    public YalsErrorBuilder addRawException(final Throwable throwable) {
         this.rawException = throwable;
         return this;
     }
 
+    /**
+     * Triggers build, which makes {@link YalsError} from {@link YalsErrorBuilder}.
+     *
+     * @return created {@link YalsError} object.
+     */
     public YalsError build() {
         YalsError yalsError = new YalsError();
         if (StringUtils.isNotBlank(techMessage)) {
