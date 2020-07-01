ac384bc58d43ff174e67dbd801574f0e4ffcb442
Alex Muravya <kyberorg@yadev.eu>
2020-07-01 12:18:28 +0300

    213 Ops tunes: HeapDump when OOM  (#213/#214)
    * Migrated to JDK
    * Added option to generate heap dump if OOM occurs 
    
    https://gist.github.com/kyberorg/32b9054ef0203d64c98d5a293d7e3cca

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
-FROM openjdk:11-jre-slim
+FROM openjdk:11-jdk
 VOLUME /tmp
 
 COPY ./target/yals.jar /app/
 COPY ./docker-entrypoint.sh /
 RUN sh -c 'chmod +x /docker-entrypoint.sh'
-RUN sh -c 'apt-get update && apt-get install -y netcat curl jq && apt -y autoremove && rm -rf /var/lib/apt/lists/*'
+RUN sh -c 'apt-get update && apt-get upgrade -y && apt-get install -y netcat curl jq && apt -y autoremove && rm -rf /var/lib/apt/lists/*'
 
 ENTRYPOINT ./docker-entrypoint.sh
 
-EXPOSE 8080 8000
+EXPOSE 8080
 
 HEALTHCHECK --start-period=60s --interval=5s --timeout=20s --retries=3 \
    CMD curl --silent --request GET http://127.0.0.1:8080/actuator/health \
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -26,7 +26,18 @@ file_env 'YALS_DB_PASSWORD'
 file_env 'TELEGRAM_TOKEN'
 file_env 'BUGSNAG_TOKEN'
 
-exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom \
---add-opens java.base/java.lang=ALL-UNNAMED \
--XX:+UseContainerSupport -XX:+AlwaysActAsServerClassMachine \
--jar /app/yals.jar
+JAVA_OPTS=${JAVA_OPTS}
+
+if [ -n "${JAVA_DEBUG_PORT}" ]; then
+  export JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:${JAVA_DEBUG_PORT}"
+fi
+
+export JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:/dev/./urandom"
+export JAVA_OPTS="$JAVA_OPTS --add-opens java.base/java.lang=ALL-UNNAMED"
+export JAVA_OPTS="$JAVA_OPTS -XX:+UseContainerSupport"
+export JAVA_OPTS="$JAVA_OPTS -XX:+AlwaysActAsServerClassMachine"
+export JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/opt/dumps"
+
+echo "Executing: java ${JAVA_OPTS} -jar /app/yals.jar"
+
+exec java ${JAVA_OPTS} -jar /app/yals.jar
