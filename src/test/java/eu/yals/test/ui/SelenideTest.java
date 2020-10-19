b0a1c4d24328efc6bb51da033f4514ac2bc495ee
Alex Muravya <kyberorg@yadev.eu>
2020-10-19 16:47:13 +0300

    UI Tests: Selenide reports to Grid

--- src/test/java/eu/yals/test/ui/SelenideTest.java
+++ src/test/java/eu/yals/test/ui/SelenideTest.java
@@ -15,7 +15,9 @@ import org.junit.platform.commons.util.StringUtils;
 import org.junit.rules.TestRule;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.Testcontainers;
 import org.testcontainers.containers.BrowserWebDriverContainer;
@@ -23,6 +25,7 @@ import org.testcontainers.containers.BrowserWebDriverContainer;
 import java.io.File;
 import java.util.Optional;
 
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_FAILING;
 
 @Slf4j
@@ -42,21 +45,36 @@ public abstract class SelenideTest {
     private final static String LOCAL_URL = String.format("http://host.testcontainers.internal:%d", SERVER_PORT);
     protected final static String BASE_URL = System.getProperty(TestApp.Properties.TEST_URL, LOCAL_URL);
 
+    private static final String BUILD_NAME =
+            System.getProperty(TestApp.Properties.BUILD_NAME, TestApp.Defaults.BUILD_NAME);
+
+    private static String testName;
+
     @Rule  // automatically takes screenshot of every failed test
     public ScreenShooter makeScreenshotOnFailure = ScreenShooter.failedTests();
-
 
     @Rule  // catching test result and triggering BrowserWebDriverContainer#afterTest() for saving test recordings
     public final TestRule watchman = new TestWatcher() {
         @Override
+        protected void starting(final Description description) {
+            super.starting(description);
+            testName = setTestNameFromTestDescription(description);
+            log.info(String.format("Starting build '%s'. Test: '%s", BUILD_NAME, testName));
+        }
+
+        @Override
         protected void succeeded(Description description) {
             super.succeeded(description);
+            Cookie cookie = new Cookie("zaleniumTestPassed", "true");
+            getWebDriver().manage().addCookie(cookie);
             chrome.afterTest(YalsTestDescription.fromDescription(description), Optional.empty());
         }
 
         @Override
         protected void failed(Throwable e, Description description) {
             super.failed(e, description);
+            Cookie cookie = new Cookie("zaleniumTestPassed", "false");
+            getWebDriver().manage().addCookie(cookie);
             chrome.afterTest(YalsTestDescription.fromDescription(description), Optional.of(e));
         }
     };
@@ -70,6 +88,9 @@ public abstract class SelenideTest {
 
         if (shouldRunTestsAtGrid()) {
             Configuration.remote = getGridFullUrl();
+
+            //to distinguish test in Grid
+            addTestNameToDriver();
             System.setProperty(TestApp.Properties.TEST_RUN_MODE, TestApp.RunMode.GRID.name());
         } else {
             //expose ports if testing local URL
@@ -133,5 +154,23 @@ public abstract class SelenideTest {
         } else {
             return HTTPS_PREFIX + gridHostname + GRID_POSTFIX;
         }
+    }
+
+    private String setTestNameFromTestDescription(final Description description) {
+        String rawMethodName = description.getMethodName();
+        String[] methodAndBrowserInfo = rawMethodName.split("\\[");
+        if (methodAndBrowserInfo.length > 0) {
+            String method = methodAndBrowserInfo[0];
+            return String.format("%s.%s", description.getTestClass().getName(), method);
+        } else {
+            return String.format("%s.%s", description.getTestClass().getName(), rawMethodName);
+        }
+    }
+
+    private static void addTestNameToDriver() {
+        DesiredCapabilities extraCapabilities = new DesiredCapabilities();
+        extraCapabilities.setCapability("name", testName);
+        extraCapabilities.setCapability("build", BUILD_NAME);
+        Configuration.browserCapabilities = extraCapabilities;
     }
 }
