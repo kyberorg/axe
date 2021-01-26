7b3a4280cea3eef44f70ae4bf76376c059e138c7
Aleksandr Muravja <root@kyberorg.io>
2021-01-26 17:40:03 +0200

    Fixing Flaky Test (#322) and testing new deploy policy (#221) (#324)
    * Updated URL in flaky test to see it app logs (#322)
    * Updated App Logic to fix flaky tests (#322)
    * Changed Var Name in Test to meet checkstyle

--- src/test/java/io/kyberorg/yalsee/test/ui/usage/AbnormalUsageTest.java
+++ src/test/java/io/kyberorg/yalsee/test/ui/usage/AbnormalUsageTest.java
@@ -28,11 +28,11 @@ public class AbnormalUsageTest extends SelenideTest {
     @Test
     public void extraArgumentsShouldBeIgnored() {
         final String EXTRA_ARGUMENT = "mineMetsa";
-        final String LINK_TO_SAVE = "https://vr.fi";
+        final String linkToSave = "https://github.com/kyberorg/yalsee/issues/322";
 
         open("/?" + EXTRA_ARGUMENT);
 
-        HomePageObject.pasteValueInFormAndSubmitIt(LINK_TO_SAVE);
+        HomePageObject.pasteValueInFormAndSubmitIt(linkToSave);
 
         HomePageObject.ResultArea.RESULT_LINK.shouldNotBe(empty);
         HomePageObject.ResultArea.RESULT_LINK.shouldNotHave(text(EXTRA_ARGUMENT));
