8af3806b4199facd6d13db30e12a8b58c27768ad
Aleksandr Muravja <alex@kyberorg.io>
2021-07-14 17:28:31 +0300

    Store API. JSON by default (#347)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

---
+++ src/main/java/io/kyberorg/yalsee/configuration/WebConfiguration.java
@@ -0,0 +1,14 @@
+package io.kyberorg.yalsee.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+}
