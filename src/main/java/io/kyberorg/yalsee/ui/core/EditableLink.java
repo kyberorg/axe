04f69a883c30ed4b6c55fc7e70d302353289116c
Aleksandr Muravja <alex@kyberorg.io>
2021-07-27 13:42:58 +0300

    MyLinkView with Grid. Editing link PoC 4 (#195)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/main/java/io/kyberorg/yalsee/ui/core/EditableLink.java
+++ src/main/java/io/kyberorg/yalsee/ui/core/EditableLink.java
@@ -1,11 +1,13 @@
 package io.kyberorg.yalsee.ui.core;
 
 import com.vaadin.flow.component.AbstractSinglePropertyField;
+import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import lombok.Getter;
 
+@Tag("editable-link")
 public class EditableLink extends AbstractSinglePropertyField<TextField, String> {
     @Getter private final Text shortDomainPart;
     @Getter private final TextField editIdentField;
