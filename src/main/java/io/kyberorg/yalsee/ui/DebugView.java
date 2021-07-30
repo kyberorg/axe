2332a3ca52ece58b0a42f5d0d167a4c5ff63c442
Aleksandr Muravja <alex@kyberorg.io>
2021-07-30 16:58:29 +0300

    MyLinkView. Session Init Listener (#195)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/main/java/io/kyberorg/yalsee/ui/DebugView.java
+++ src/main/java/io/kyberorg/yalsee/ui/DebugView.java
@@ -4,6 +4,9 @@ import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.ServiceException;
+import com.vaadin.flow.server.SessionInitEvent;
+import com.vaadin.flow.server.SessionInitListener;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
 import io.kyberorg.yalsee.Endpoint;
@@ -13,7 +16,7 @@ import io.kyberorg.yalsee.ui.core.YalseeLayout;
 @UIScope
 @Route(value = Endpoint.UI.DEBUG_PAGE, layout = MainView.class)
 @PageTitle("Yalsee: Debug Page")
-public class DebugView extends YalseeLayout {
+public class DebugView extends YalseeLayout implements SessionInitListener {
 
     /**
      * Creates {@link DebugView}.
@@ -22,5 +25,11 @@ public class DebugView extends YalseeLayout {
         setId(DebugView.class.getSimpleName());
         add(new H2("Debug Page"));
         add(new Text("Ready to debug something..."));
+    }
+
+    @Override
+    public void sessionInit(SessionInitEvent event) throws ServiceException {
+        add(new Text("Session ID:" + event.getSession().getSession().getId()));
+        add(new Text("Created: " + event.getSession().getSession().getCreationTime()));
     }
 }
