16da7b75e36e0f796b21ec25e2a7e3f66bf6c6b9
Alex Muravya <asm@virtalab.net>
2021-07-31 13:51:55 +0300

    Sessions PoC. Sched (#195)

---
+++ src/main/java/io/kyberorg/yalsee/configuration/SpringConfig.java
@@ -0,0 +1,9 @@
+package io.kyberorg.yalsee.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SpringConfig {
+}
