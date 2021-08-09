db491c26ea73ec7507d9ae2818bf80ea28f27d45
Aleksandr Muravja <alex@kyberorg.io>
2021-08-09 14:24:17 +0300

    Improve test by waiting for Splash Screen is gone in VaadinPageObject (#456)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/test/java/io/kyberorg/yalsee/test/pageobjects/VaadinPageObject.java
+++ src/test/java/io/kyberorg/yalsee/test/pageobjects/VaadinPageObject.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.SelenideElement;
 
 import java.time.Duration;
 
+import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -15,11 +16,13 @@ import static com.codeborne.selenide.Selenide.$;
  */
 public final class VaadinPageObject {
     public static final SelenideElement LOADING_BAR = $(".v-loading-indicator");
+    public static final SelenideElement SPLASH_SCREEN = $("#splash-screen");
 
     /**
      * Ensures that site is loaded and Vaadin loading bar already disappear.
      */
     public static void waitForVaadin() {
+        $(SPLASH_SCREEN).shouldHave(cssClass("loaded"), Duration.ofMillis(Configuration.timeout));
         $(LOADING_BAR).shouldNotBe(visible, Duration.ofMillis(Configuration.timeout));
     }
 
