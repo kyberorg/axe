577dba197d89ae78e8080f7f3a78c439bde6409e
Aleksandr Muravja <alex@kyberorg.io>
2021-08-09 16:14:46 +0300

    Replace Olli's Clipboard Helper with ClipboardUtils (#444)
    * Replace Olli's Clipboard Helper with ClipboardUtils (#444)
    
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

---
+++ src/main/java/io/kyberorg/yalsee/utils/ClipboardUtils.java
@@ -0,0 +1,52 @@
+package io.kyberorg.yalsee.utils;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.spring.annotation.UIScope;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Utils for operating with Clipboard.
+ * <p>
+ *
+ * @see https://cookbook.vaadin.com/copy-to-clipboard
+ * @since 3.1.2
+ */
+@Slf4j
+@UIScope
+@JsModule("./js/copytoclipboard.js")
+public final class ClipboardUtils {
+
+    private ClipboardUtils() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    /**
+     * Copies text and shows Notification with given text and {@link Notification.Position}.
+     *
+     * @param textToCopy           string with text to copy to clipboard
+     * @param notificationText     string with notification text
+     * @param notificationPosition position of notification
+     */
+    public static void copyToClipboardAndNotify(final String textToCopy, final String notificationText,
+                                                final Notification.Position notificationPosition) {
+        copyToClipboard(textToCopy);
+        getLinkCopiedNotification(notificationText, notificationPosition).open();
+    }
+
+    private static void copyToClipboard(final String textToCopy) {
+        UI.getCurrent().getPage().executeJs("window.copyToClipboard($0)", textToCopy);
+    }
+
+    private static Notification getLinkCopiedNotification(final String notificationText,
+                                                          final Notification.Position notificationPosition) {
+        final int notificationDuration = 3000;
+        Notification notification =
+                new Notification(notificationText, notificationDuration, notificationPosition);
+        notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
+        return notification;
+    }
+
+}
