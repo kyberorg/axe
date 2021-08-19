c18e5fe5d72997cbf10f6481d12ced061c3507c8
Aleksandr Muravja <alex@kyberorg.io>
2021-08-19 11:22:54 +0300

    `UrlUtils.decodeUrl(String)` should throw more concrete exception (#442)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

---
+++ src/main/java/io/kyberorg/yalsee/exception/URLDecodeException.java
@@ -0,0 +1,17 @@
+package io.kyberorg.yalsee.exception;
+
+/**
+ * Runtime exception that thrown when application it failed to decode URL.
+ *
+ * @since 3.2.1
+ */
+public class URLDecodeException extends RuntimeException {
+    /**
+     * Exception Constructor.
+     *
+     * @param cause UnsupportedEncodingException
+     */
+    public URLDecodeException(final Throwable cause) {
+        super(cause);
+    }
+}
