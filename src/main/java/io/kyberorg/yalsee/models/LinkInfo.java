10d84f54292ef38835ff13304aecd5db9afccbb8
Aleksandr Muravja <alex@kyberorg.io>
2021-10-07 12:49:14 +0300

    Lombok's `@Data` replaced with other annotations in `@Entity` classes aka models (#520)
    * Lombok's @Data replaced with other annotations in @Entity annotated classes aka models. (#520)
    
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>
    
    * Hash code fix (#520)
    
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/main/java/io/kyberorg/yalsee/models/LinkInfo.java
+++ src/main/java/io/kyberorg/yalsee/models/LinkInfo.java
@@ -1,18 +1,24 @@
 package io.kyberorg.yalsee.models;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.Objects;
 
 /**
  * LinkInfo table mapped Java Object.
  *
  * @since 3.2
  */
-@Data
-@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(name = "link_info")
 public class LinkInfo {
@@ -40,4 +46,17 @@ public class LinkInfo {
 
     @Column(name = UPDATED_COLUMN, nullable = false)
     private Timestamp updated;
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        LinkInfo linkInfo = (LinkInfo) o;
+        return Objects.equals(id, linkInfo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ident, description, session);
+    }
 }
