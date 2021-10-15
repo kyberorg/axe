6d4e15dbee90b77a308afc3aa6f28aba6dcd5881
Aleksandr Muravja <alex@kyberorg.io>
2021-10-15 13:33:39 +0300

    LoginView. 2FA sending fix. (#511)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/main/java/io/kyberorg/yalsee/services/user/verificationsenders/NoOpVerificationSender.java
+++ src/main/java/io/kyberorg/yalsee/services/user/verificationsenders/NoOpVerificationSender.java
@@ -1,7 +1,9 @@
 package io.kyberorg.yalsee.services.user.verificationsenders;
 
 import io.kyberorg.yalsee.result.OperationResult;
+import org.springframework.stereotype.Component;
 
+@Component
 public class NoOpVerificationSender implements VerificationSender {
     @Override
     public OperationResult sendVerification(String destination, String verificationCode) {
