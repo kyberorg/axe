10640f22b81df0aabe80b0d9d807b2201b375bbe
Aleksandr Muravja <alex@kyberorg.io>
2021-10-28 12:18:08 +0300

    Base image: improved stability by mirroring golang image (#560)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- docker/base-image/create-jre.sh
+++ docker/base-image/create-jre.sh
@@ -1,1 +1,3 @@
+docker pull gcr.io/distroless/java-debian11:11-nonroot
 docker build -t quay.io/kyberorg/yalsee-base:distroless-jre-11 -f Dockerfile.jre .
+docker push quay.io/kyberorg/yalsee-base:distroless-jre-11
