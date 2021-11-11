33a826ac685d09d853c8efaffac7b5656fd4cccc
Aleksandr Muravja <alex@kyberorg.io>
2021-11-11 11:26:35 +0200

    Allow numbers in ident (#537)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/main/java/io/kyberorg/yalsee/core/IdentGenerator.java
+++ src/main/java/io/kyberorg/yalsee/core/IdentGenerator.java
@@ -10,7 +10,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 public final class IdentGenerator {
     private static final int IDENT_DEFAULT_LENGTH = 6;
     private static final int IDENT_MAX_LENGTH = 255;
-    public static final String VALID_IDENT_PATTERN = "[a-zA-Z]{2," + IDENT_MAX_LENGTH + "}";
+    public static final String VALID_IDENT_PATTERN = "[a-zA-Z0-9]{2," + IDENT_MAX_LENGTH + "}";
 
     private IdentGenerator() {
         throw new UnsupportedOperationException("Utility class");
