f08151535f68b0fb1035b58762e563fb1eded038
Aleksandr Muravja <alex@kyberorg.io>
2021-11-19 14:05:14 +0200

    Improve `TimeModel` by adding update method and defaults. Fix (#601)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/main/java/io/kyberorg/yalsee/models/dao/base/TimeRepositoryImpl.java
+++ src/main/java/io/kyberorg/yalsee/models/dao/base/TimeRepositoryImpl.java
@@ -1,21 +1,27 @@
 package io.kyberorg.yalsee.models.dao.base;
 
 import io.kyberorg.yalsee.models.TimeModel;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import javax.persistence.EntityManager;
 
-@RequiredArgsConstructor
-@Repository
+@NoRepositoryBean
 @Transactional
-public class TimeRepositoryImpl<T extends TimeModel, ID> implements TimeRepository<T, ID> {
+public class TimeRepositoryImpl<T extends TimeModel, ID> extends SimpleJpaRepository<T, ID>
+        implements TimeRepository<T, ID> {
 
     private final JpaEntityInformation<T, ?> entityInformation;
     private final EntityManager em;
+
+    public TimeRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
+        super(entityInformation, entityManager);
+        this.entityInformation = entityInformation;
+        this.em = entityManager;
+    }
 
     public T save(T entity) {
         Assert.notNull(entity, "Entity must not be null.");
