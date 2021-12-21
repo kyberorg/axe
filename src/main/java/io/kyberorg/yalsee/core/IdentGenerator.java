b8b5e7b55cd4112d4921d9d2534fce8559eadfcd
Aleksandr Muravja <alex@kyberorg.io>
2021-12-21 16:20:24 +0200

    Allowed point and underscore in idents. Code fixes (#616)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/main/java/io/kyberorg/yalsee/core/IdentGenerator.java
+++ src/main/java/io/kyberorg/yalsee/core/IdentGenerator.java
@@ -13,6 +13,7 @@ public final class IdentGenerator {
     private static final int IDENT_MAX_LENGTH_WITHOUT_FIRST_AND_LAST_CHARS = IDENT_MAX_LENGTH - 2;
 
     /**
+     * Ident pattern.
      * Regexp explanation:
      * ^[a-zA-Z0-9]      # start with an alphanumeric character
      * (                 # start of (group 1)
@@ -27,7 +28,7 @@ public final class IdentGenerator {
      * # {0,IDENT_MAX_LENGTH - 2} plus the first and last alphanumeric characters,
      * # total length became {2, IDENT_MAX_LENGTH}
      */
-    public static final String VALID_IDENT_PATTERN = "[a-zA-Z0-9]"
+    public static final String VALID_IDENT_PATTERN = "^[a-zA-Z0-9]"
             + "([._-](?![._-])|[a-zA-Z0-9]){0," + IDENT_MAX_LENGTH_WITHOUT_FIRST_AND_LAST_CHARS + "} "
             + "[a-zA-Z0-9]$";
 
