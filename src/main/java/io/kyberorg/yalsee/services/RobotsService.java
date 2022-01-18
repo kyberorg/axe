ade27ce6fbf6cb2b48274d034ba190e4e5ae6dc2
Aleksandr Muravja <alex@kyberorg.io>
2022-01-18 09:46:01 +0200

    Made robots list Redis_Disabled friendly (#705) (#708)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/main/java/io/kyberorg/yalsee/services/RobotsService.java
+++ src/main/java/io/kyberorg/yalsee/services/RobotsService.java
@@ -3,9 +3,12 @@ package io.kyberorg.yalsee.services;
 import io.kyberorg.yalsee.models.dao.RobotsRedisDao;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * List of automated software, which makes single request per time.
@@ -15,9 +18,19 @@ import javax.annotation.PostConstruct;
 @RequiredArgsConstructor
 @Service
 public final class RobotsService {
+    private static final List<String> ROBOTS_LIST = new ArrayList<>();
+
+    static {
+        ROBOTS_LIST.add("curl");
+        ROBOTS_LIST.add("wget");
+    }
+
     private static RobotsService instance;
 
     private final RobotsRedisDao robotsRedisDao;
+
+    @Value("${redis.enabled}")
+    private boolean isRedisEnabled;
 
     /**
      * Makes {@link RobotsService} be accessible from static context aka POJO.
@@ -41,8 +54,14 @@ public final class RobotsService {
      */
     public boolean isRobot(final String userAgent) {
         if (StringUtils.isBlank(userAgent)) return true; //device with empty UA isn't valid client.
+        List<String> robotList;
+        if (isRedisEnabled) {
+            robotList = robotsRedisDao.getRobots();
+        } else {
+            robotList = ROBOTS_LIST;
+        }
         boolean isRobot = false;
-        for (String robot : robotsRedisDao.getRobots()) {
+        for (String robot : robotList) {
             if (userAgent.contains(robot)) {
                 isRobot = true;
                 break;
