54b9a59d35cbe71af4c145de9c1ed48c2de13591
Aleksandr Muravja <alex@kyberorg.io>
2022-01-26 09:57:17 +0200

    Write to File is done. Fail emulation. Go program: maven version and show command (#507)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- mvn-rr.go
+++ mvn-rr.go
@@ -3,39 +3,87 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 )
 
-const EmptyFile = "EMPTY_FILE"
-const EmptyOpts = ""
+const (
+	EmptyFile   = "EMPTY_FILE"
+	EmptyString = ""
+)
+
+var (
+	failedTests      []string
+	rerunCommandArgs []string
+)
 
 func main() {
 	failedTestsList := flag.String("file", EmptyFile, "File with failed test")
-	mvnExtraParams := flag.String("mavenOpts", EmptyOpts, "Maven params aka -D flags")
+	mvnProfiles := flag.String("profiles", EmptyString, "Maven Profiles (if any)")
+	mvnExtraParams := flag.String("params", EmptyString, "Maven params aka -D flags")
+	mvnTargets := flag.String("targets", "test", "Space-separated maven targets")
 
 	flag.Parse()
 
 	if *failedTestsList == EmptyFile {
-		log.Fatal("No file with failed test defined. Please use '-file /path/to/file' flag")
+		log.Fatal("No file with failed test defined. Please use '-file /path/to/file' flag.")
 	}
+	if strings.Contains(*mvnExtraParams, "-Dtest") {
+		log.Fatal("Conflict: mvn-rr constructs own -Dtest and your version would be overridden. " +
+			"This is not what your want. Use plain maven instead.")
+	}
+
+	if *mvnTargets == EmptyString {
+		log.Fatalf("No maven targets defined. Nothing to do.")
+	}
+
+	profiles := strings.TrimSpace(*mvnProfiles)
+	params := strings.TrimSpace(*mvnExtraParams)
+	targets := strings.TrimSpace(*mvnTargets)
+
 	file, err := os.Open(*failedTestsList)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	var failedTests string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		test := scanner.Text()
 		if isTestNameValid(test) {
-			failedTests += test + " "
+			appendFailedTest(test)
 		}
 	}
 
-	log.Printf("Got extra args: %s, Got tests: %s", *mvnExtraParams, failedTests)
+	mvnCmd := "mvn"
+	versionArgs := "--version"
+	versionCommand := exec.Command(mvnCmd, versionArgs)
+	versionOut, err := versionCommand.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Running with \n%s\n", string(versionOut))
+
+	if profiles != EmptyString {
+		appendCommandArgs("-P=" + profiles)
+	}
+	if params != EmptyString {
+		appendCommandArgs(params)
+	}
+	if len(failedTests) > 0 {
+		appendCommandArgs("-Dtest=\"")
+		for _, failedTest := range failedTests {
+			appendCommandArgs(failedTest + " ")
+		}
+		appendCommandArgs("\"")
+	}
+	appendCommandArgs(targets)
+
+	fmt.Printf("%s %s", mvnCmd, rerunCommandArgs)
 }
 
 func isTestNameValid(testName string) bool {
@@ -60,4 +108,12 @@ func isTestNameValid(testName string) bool {
 	} else {
 		return false
 	}
+}
+
+func appendFailedTest(failedTest string) {
+	failedTests = append(failedTests, failedTest)
+}
+
+func appendCommandArgs(arg string) {
+	rerunCommandArgs = append(rerunCommandArgs, arg)
 }
