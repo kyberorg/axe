df1f5cf293806508785ed9b73e19207e5c56fd95
Aleksandr Muravja <alex@kyberorg.io>
2022-02-02 13:53:36 +0200

    Clickable logo at App Menu (#604) (#747)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/main/java/io/kyberorg/yalsee/ui/MainView.java
+++ src/main/java/io/kyberorg/yalsee/ui/MainView.java
@@ -209,8 +209,11 @@ public class MainView extends AppLayout implements BeforeEnterObserver, PageConf
         Image logo = new Image("/images/logo.png", "Icon");
         logo.setId(APP_LOGO);
         logo.addClassName("logo-image");
-        Tab logoTab = new Tab(logo);
-        logoTab.setEnabled(false);
+
+        RouterLink logoLink = new RouterLink(null, HomeView.class);
+        logoLink.add(logo);
+
+        Tab logoTab = new Tab(logoLink);
         tabs.add(logoTab);
     }
 
