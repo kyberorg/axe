8cbba4703a1bf6ef136177a8649d015307d784ff
Aleksandr Muravja <alex@kyberorg.io>
2022-05-20 14:52:03 +0300

    Migrate Base image from quay.io to kio.ee (#864)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- docker/base-image/create-jdk.sh
+++ docker/base-image/create-jdk.sh
@@ -1,3 +1,5 @@
+docker login kio.ee
 docker pull gcr.io/distroless/java17-debian11:debug-nonroot
-docker build -t quay.io/kyberorg/yalsee-base:distroless-jdk-17 -f Dockerfile.jdk .
-docker push quay.io/kyberorg/yalsee-base:distroless-jdk-17
+docker build -t kio.ee/kyberorg/yalsee-base:distroless-jdk-17 -f Dockerfile.jdk .
+docker push kio.ee/kyberorg/yalsee-base:distroless-jdk-17
+docker logout kio.ee
