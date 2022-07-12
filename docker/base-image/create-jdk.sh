7b1d305da5ed7e2fce4a111c2a14646bafaeba47
Aleksandr Muravja <alex@kyberorg.io>
2022-07-12 12:56:04 +0300

    Docker Base Image: Distroless -> Alpine/Amazoncorretto (#882) (#908)

--- docker/base-image/create-jdk.sh
+++ docker/base-image/create-jdk.sh
@@ -1,5 +1,6 @@
 docker login kio.ee
-docker pull gcr.io/distroless/java17-debian11:debug-nonroot
-docker build -t kio.ee/kyberorg/yalsee-base:distroless-jdk-17 -f Dockerfile.jdk .
-docker push kio.ee/kyberorg/yalsee-base:distroless-jdk-17
+docker pull kio.ee/lib/golang:1.18.3-alpine
+docker pull kio.ee/lib/eclipse-temurin:17-jdk-alpine
+docker build -t kio.ee/yalsee/base:alpine-jdk-17 -f Dockerfile.jdk .
+docker push kio.ee/yalsee/base:alpine-jdk-17
 docker logout kio.ee
