f23e0078203e0543d1a47e3e6440e6454542b446
Aleksandr Muravja <alex@kyberorg.io>
2022-07-12 12:43:25 +0300

    Docker Base Image: Distroless -> Alpine/Amazoncorretto (#882)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- docker/base-image/create-golang.sh
+++
@@ -1,5 +0,0 @@
-docker login kio.ee
-docker pull golang:1.18.2-bullseye
-docker build -t kio.ee/kyberorg/golang:1.18.2 -f Dockerfile.golang .
-docker push kio.ee/kyberorg/golang:1.18.2
-docker logout kio.ee
