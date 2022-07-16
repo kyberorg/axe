b4c4c91d7e3b2f9e89a3ba9e84a3b4b64e53c97e
Aleksandr Muravja <alex@kyberorg.io>
2022-07-16 23:25:41 +0300

    Yalsee Base images updated (#914) (#916)
    * Update base images (#914)
    
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>
    
    * Update base images. Pull base and build images before building Yalsee (#914)
    
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- src/main/java/io/kyberorg/yalsee/utils/ApiUtils.java
+++ src/main/java/io/kyberorg/yalsee/utils/ApiUtils.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
  * API tools and helper methods.
  */
 public final class ApiUtils {
-
     private ApiUtils() {
         throw new UnsupportedOperationException("Utility class");
     }
