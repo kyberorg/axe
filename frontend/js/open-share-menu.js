f9d3aed7b82a81c133c3bf4f40623586f7aa4447
Aleksandr Muravja <alex@kyberorg.io>
2022-09-28 13:56:01 +0300

    Share Menu. Mobile edition. Fix JS 2 (#734)
    Signed-off-by: Aleksandr Muravja <alex@kyberorg.io>

--- frontend/js/open-share-menu.js
+++ frontend/js/open-share-menu.js
@@ -1,5 +1,16 @@
-window.openShareMenu = (content) => {
-    window.navigator.share(content)
+window.openShareMenu = (link, description) => {
+    let titleString;
+    if (description) {
+        titleString = description;
+    } else {
+        titleString = link;
+    }
+
+    window.navigator.share({
+        title: titleString,
+        text: description,
+        url: link,
+    })
         .then(() => console.log("Item shared"))
         .catch(() => console.error("Failed to share link"));
 }
