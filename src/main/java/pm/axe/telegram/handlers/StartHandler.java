8a7f3be7aac5017921a7c564bdccf4a91bc75c0d
Aleksandr Muravja <aleks@kyberorg.pm>
2022-12-27 14:29:47 +0200

    Telegram Stuff rewritten. Fixes (#1043)
    Signed-off-by: Aleksandr Muravja <aleks@kyberorg.pm>

--- src/main/java/pm/axe/telegram/handlers/StartHandler.java
+++ src/main/java/pm/axe/telegram/handlers/StartHandler.java
@@ -1,13 +1,21 @@
 package pm.axe.telegram.handlers;
 
+import com.vdurmont.emoji.EmojiParser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import pm.axe.Axe;
+import pm.axe.services.telegram.TelegramService;
 
+@RequiredArgsConstructor
 @Component
-public class StartHandler implements TelegramCommandHandler{
+public class StartHandler implements TelegramCommandHandler {
+    private final TelegramService telegramService;
     @Override
-    public String handle(Update update) {
-        //FIXME impl
-        return null;
+    public String handle(final Update update) {
+        final String userName = update.getMessage().getFrom().getUserName();
+        final String greetings = EmojiParser.parseToUnicode(String.format("%s Hello, %s!", Axe.Emoji.HELLO, userName));
+        final String usage = telegramService.usage();
+        return greetings + Axe.C.NEW_LINE + usage;
     }
 }
