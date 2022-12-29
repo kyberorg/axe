d9b37cfee40a0fba672f82a3785f5e406e12265c
Aleksandr Muravja <aleks@kyberorg.pm>
2022-12-29 23:08:27 +0200

    Try iOS spec JS. Clipboard API (#1122)
    Signed-off-by: Aleksandr Muravja <aleks@kyberorg.pm>

--- frontend/js/copy-to-clipboard.js
+++ frontend/js/copy-to-clipboard.js
@@ -1,23 +1,32 @@
 window.copyToClipboard = (str) => {
-    const textarea = document.createElement("textarea");
-    textarea.value = str;
-    textarea.style.position = "absolute";
-    textarea.style.opacity = "0";
-    document.body.appendChild(textarea);
-
-    if(isOS()) {
-        let range = document.createRange()
-        range.selectNodeContents(textarea)
-        let selection = window.getSelection()
-        selection.removeAllRanges()
-        selection.addRange(range);
-        textarea.setSelectionRange(0, 999999);
+    //using new Clipboard API
+    if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
+        navigator.clipboard.writeText(str).then(
+            () => console.log("Copied text OK"),
+            (e) => console.log("Failed to copy text",e)
+        );
     } else {
-        input.select()
-    }
+        //fallback to deprecated stuff
+        const textarea = document.createElement("textarea");
+        textarea.value = str;
+        textarea.style.position = "absolute";
+        textarea.style.opacity = "0";
+        document.body.appendChild(textarea);
 
-    document.execCommand("copy")
-    document.body.removeChild(textarea);
+        if(isOS()) {
+            let range = document.createRange()
+            range.selectNodeContents(textarea)
+            let selection = window.getSelection()
+            selection.removeAllRanges()
+            selection.addRange(range);
+            textarea.setSelectionRange(0, 999999);
+        } else {
+            input.select()
+        }
+
+        document.execCommand("copy")
+        document.body.removeChild(textarea);
+    }
 };
 
 function isOS() {
