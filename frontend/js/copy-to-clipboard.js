666affb5334d7197f284b76652fc3f4fda7b797b
Aleksandr Muravja <aleks@kyberorg.pm>
2022-12-29 23:44:37 +0200

    Try iOS spec JS. SO (#1122)
    Signed-off-by: Aleksandr Muravja <aleks@kyberorg.pm>

--- frontend/js/copy-to-clipboard.js
+++ frontend/js/copy-to-clipboard.js
@@ -1,49 +1,55 @@
 window.copyToClipboard = (str) => {
-    Clipboard.copy(str);
+    c2c(str)
 }
 
-window.Clipboard = (function(window, document, navigator) {
-    let textArea, copy;
+/**
+ * Copy a string to clipboard.
+ *
+ * @param  {String} string         The string to be copied to clipboard
+ * @return {Boolean}               returns a boolean correspondent to the success of the copy operation.
+ * @see https://stackoverflow.com/a/53951634/938822
+ */
 
-    function isOS() {
-        return navigator.userAgent.match(/ipad|iphone/i);
+function c2c(string) {
+    let textarea;
+    let result;
+
+    try {
+        textarea = document.createElement('textarea');
+        textarea.setAttribute('readonly', true);
+        textarea.setAttribute('contenteditable', true);
+        textarea.style.position = 'fixed'; // prevent scroll from jumping to the bottom when focus is set.
+        textarea.value = string;
+
+        document.body.appendChild(textarea);
+
+        textarea.focus();
+        textarea.select();
+
+        const range = document.createRange();
+        range.selectNodeContents(textarea);
+
+        const sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+
+        textarea.setSelectionRange(0, textarea.value.length);
+        result = document.execCommand('copy');
+    } catch (err) {
+        console.error(err);
+        result = null;
+    } finally {
+        document.body.removeChild(textarea);
     }
 
-    function createTextArea(text) {
-        textArea = document.createElement('textArea');
-        textArea.value = text;
-        document.body.appendChild(textArea);
-    }
-
-    function selectText() {
-        var range,
-            selection;
-
-        if (isOS()) {
-            range = document.createRange();
-            range.selectNodeContents(textArea);
-            selection = window.getSelection();
-            selection.removeAllRanges();
-            selection.addRange(range);
-            textArea.setSelectionRange(0, 999999);
-        } else {
-            textArea.select();
+    // manual copy fallback using prompt
+    if (!result) {
+        const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+        const copyHotkey = isMac ? '⌘C' : 'CTRL+C';
+        result = prompt(`Press ${copyHotkey}`, string); // eslint-disable-line no-alert
+        if (!result) {
+            return false;
         }
     }
-
-    function copyToClipboard() {
-        document.execCommand('copy');
-        document.body.removeChild(textArea);
-    }
-
-    copy = function(text) {
-        createTextArea(text);
-        selectText();
-        copyToClipboard();
-    };
-
-    return {
-        copy: copy
-    };
-})(window, document, navigator);
-
+    return true;
+}
