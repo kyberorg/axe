e78a0942a15496ebe43fab4607f7e9ea43149a47
Aleksandr Muravja <aleks@kyberorg.pm>
2023-01-06 17:44:23 +0200

    Olli Teitäväisen Clipboard script (#1122)
    Signed-off-by: Aleksandr Muravja <aleks@kyberorg.pm>

--- frontend/js/copy-to-clipboard.js
+++ frontend/js/copy-to-clipboard.js
@@ -1,55 +1,21 @@
 window.copyToClipboard = (str) => {
-    c2c(str)
-}
+    const textarea = document.createElement('textarea');
+    textarea.value = str
+    textarea.setAttribute('readonly', '');
+    textarea.style.position = 'absolute';
+    textarea.style.left = '-9999px';
 
-/**
- * Copy a string to clipboard.
- *
- * @param  {String} string         The string to be copied to clipboard
- * @return {Boolean}               returns a boolean correspondent to the success of the copy operation.
- * @see https://stackoverflow.com/a/53951634/938822
- */
+    document.body.appendChild(textarea);
 
-function c2c(string) {
-    let textarea;
-    let result;
-
-    try {
-        textarea = document.createElement('textarea');
-        textarea.setAttribute('readonly', true);
-        textarea.setAttribute('contenteditable', true);
-        textarea.style.position = 'fixed'; // prevent scroll from jumping to the bottom when focus is set.
-        textarea.value = string;
-
-        document.body.appendChild(textarea);
-
-        textarea.focus();
-        textarea.select();
-
-        const range = document.createRange();
-        range.selectNodeContents(textarea);
-
-        const sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
-
-        textarea.setSelectionRange(0, textarea.value.length);
-        result = document.execCommand('copy');
-    } catch (err) {
-        console.error(err);
-        result = null;
-    } finally {
-        document.body.removeChild(textarea);
+    const selected =
+        document.getSelection().rangeCount > 0
+            ? document.getSelection().getRangeAt(0)
+            : false;
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    if (selected) {
+        document.getSelection().removeAllRanges();
+        document.getSelection().addRange(selected);
     }
-
-    // manual copy fallback using prompt
-    if (!result) {
-        const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
-        const copyHotkey = isMac ? '⌘C' : 'CTRL+C';
-        result = prompt(`Press ${copyHotkey}`, string); // eslint-disable-line no-alert
-        if (!result) {
-            return false;
-        }
-    }
-    return true;
 }
